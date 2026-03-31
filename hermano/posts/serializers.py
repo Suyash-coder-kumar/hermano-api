a9b470f338932ba4b43ca39d2a710b83175de0e2
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-31 17:32:55 +0530

    feat(post): serialize model and add viewset

---
+++ hermano/posts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers 
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model=Post
+        fields=['id','user','media','caption','created_at']
--- hermano/posts/views.py
+++ hermano/posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets,permissions,generics
+from .models import Post
+from .serializers import PostSerializer
 # Create your views here.
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset=Post.objects.all().order_by('-created_at')
+    serializer_class=PostSerializer()
+    permission_classes=[permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+
+class PostSearchView(generics.ListAPIView):
+    serializer_class=PostSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        query=self.request.query_params.get('q','')
+        return Post.objects.filter(caption__icontains=query).order_by('-created_at')
