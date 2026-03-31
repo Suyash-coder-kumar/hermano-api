a08c8913360ca123a9556f0bf2249c350f77c30a
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-31 17:20:47 +0530

    feat(interactions): add app for interactions

---
+++ interections/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Comment(models.Model):
+    pass
+
+class Likes(models.Model):
+    pass
+
+class Follow(models.Model):
+    pass
